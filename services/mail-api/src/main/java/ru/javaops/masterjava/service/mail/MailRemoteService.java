22bd22f8842d475190f472829db9c0a1e8de337f
sergey <Ablaev.Sergey@gmail.com>
2019-01-03 01:46:09 +0500

    1) Clean current DEV branch as New!

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailRemoteService.java
+++
@@ -1,8 +0,0 @@
-package ru.javaops.masterjava.service.mail;
-
-import ru.javaops.masterjava.service.mail.util.MailUtils;
-
-public interface MailRemoteService {
-
-    scala.concurrent.Future<GroupResult> sendBulk(MailUtils.MailObject mailObject);
-}
